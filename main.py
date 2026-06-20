43b94ead5005bc65921045c875662699864d7b6f
Sergio Gomez <sergioferg.2003@gmail.com>
2026-06-20 10:49:34 -0400

    guardar graficos

--- main.py
+++ main.py
@@ -21,10 +21,20 @@ def main():
 
     placa_con = construir_placa_completa(u_con)
     placa_sin = construir_placa_completa(u_sin)
-    #plt.spy(A, markersize=1)
+    plt.spy(A_con_fc, markersize=1)
 
-    #plt.title("Patrón de Bandas de la Matriz A")
-    #plt.show()
+    #plt.title("Patrón de Bandas de la Matriz A", fontsize=10, pad=10, fontweight='bold')
+    plt.savefig("bandas.pdf")
+
+    placa_sin[0, 0] = (100 + 50) / 2
+    placa_sin[0, -1] = (100 + 50) / 2   
+    placa_sin[-1, 0] = (0 + 50) / 2     
+    placa_sin[-1, -1] = (0 + 50) / 2
+
+    placa_con[0, 0] = (100 + 50) / 2
+    placa_con[0, -1] = (100 + 50) / 2   
+    placa_con[-1, 0] = (0 + 50) / 2     
+    placa_con[-1, -1] = (0 + 50) / 2
 
 
     colores = ["white", "yellow", "orange", "red"]
@@ -34,33 +44,38 @@ def main():
     X, Y = np.meshgrid(x, y)
 
     # con fuente calor
-    plt.figure(figsize=(10, 5))
-    mapa_con = plt.contourf(X, Y, placa_con, levels=20, cmap=mi_paleta, vmin=0, vmax=100)
-    cbar_con= plt.colorbar(mapa_con)
-    cbar_con.ax.set_title('°C', fontsize=10, pad=10, fontweight='bold')
-    plt.title("Distribución de Temperatura con Fuente Central", fontweight='bold', pad=15)
-    plt.gca().invert_yaxis()
-    plt.show()
+    plt.figure(figsize=(7, 6))
+    mapa = plt.contourf(X, Y, placa_con, levels=20, cmap=mi_paleta, vmin=0, vmax=100)
+    mapa.set_edgecolor("face")
+    mapa.set_linewidth(0.1)
+    plt.gca().set_aspect('equal')
+    cbar = plt.colorbar(mapa)
+    cbar.ax.set_title('°C', fontsize=10, pad=10, fontweight='bold')
+    #plt.title("Distribución de Temperatura con Fuente Central", fontweight='bold', pad=15)
+    plt.gca().invert_yaxis() 
+    plt.savefig("distribucion_temp.pdf")
+
     #sin fuente de calor
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(7, 6))
     mapa_sin = plt.contourf(X, Y, placa_sin, levels=20, cmap=mi_paleta, vmin=0, vmax=100)
     cbar = plt.colorbar(mapa_sin)
+    plt.gca().set_aspect('equal')
     cbar.ax.set_title('°C', fontsize=10, pad=10, fontweight='bold')
-    plt.title("Distribución de Temperatura sin Fuente Central", fontweight='bold', pad=15)
+    #plt.title("Distribución de Temperatura sin Fuente Central", fontweight='bold', pad=15)
     plt.gca().invert_yaxis()
-    plt.show()
+    plt.savefig("distribucion_temp_x.pdf")
 
     #  u(x, 0.5)
     plt.figure(figsize=(10, 5))
     indice_centro = (NUM_NODOS + 2) // 2
     perfil_transversal = placa_con[indice_centro, :]
     plt.plot(x, perfil_transversal, color='red', marker='o', linestyle='-', linewidth=2)
-    plt.title("Perfil Transversal de Temperatura para y = 0.5", fontweight='bold')
+    #plt.title("Perfil Transversal de Temperatura para y = 0.5", fontweight='bold')
     plt.xlabel("Posición x")
     plt.ylabel("Temperatura (°C)")
     plt.grid(True, linestyle='--', alpha=0.5)
     plt.ylim(0, 110)
-    plt.show()
+    plt.savefig("transversal.pdf")
 
 def construir_placa_completa(u_internos):
     placa = np.zeros((NUM_NODOS + 2, NUM_NODOS + 2))
@@ -92,41 +107,6 @@ def calcular_Ab_eficiente(con_fuente=True):
     b = b_2d.flatten()
 
     return A_sparse, b
-
-def calcular_Ab_ineficiente(con_fuente=True):
-    A = np.zeros((INCOGNITAS, INCOGNITAS))
-    b = np.zeros(INCOGNITAS)
-    k = 0
-    for i in range(0, NUM_NODOS):
-        for j in range(0, NUM_NODOS):
-            A[k][k] = 4
-
-            if i-1 < 0:
-                b[k] += 100
-            else:
-                A[k][k-NUM_NODOS] = -1
-
-            if i+1 > 19:
-                b[k] += 0
-            else:
-                A[k][k+NUM_NODOS] = -1
-            
-            if j-1 < 0:
-                b[k] += 50
-            else:
-                A[k][k-1] = -1
-
-            if j+1 > 19:
-                b[k] += 50
-            else:
-                A[k][k+1] = -1
-
-            if con_fuente and (i == 9 or i == 10) and (j == 9 or j == 10):
-                b[k] += (h ** 2) * (ENERGIA_F/4)
-
-            k += 1
-        
-    return A, b
 
 if __name__ == "__main__":
     main()
